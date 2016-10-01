c3b1d9d9fbbee4665720a1d649a3f197b2a02341
MilenaMalysh <milenamalush1@gmail.com>
2016-10-02 01:06:24 +0300

    gta serializer added

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
-//
-// Created by Ivan Prymak on 9/20/2016.
-//
+#include "tgaimage.h"
 
-int main(){
-    return 0;
+const TGAColor white = TGAColor(255, 255, 255, 255);
+const TGAColor red   = TGAColor(255, 0,   0,   255);
+
+int main(int argc, char** argv) {
+	TGAImage image(100, 100, TGAImage::RGB);
+	image.set(52, 41, red);
+	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+	image.write_tga_file("output.tga");
+	return 0;
 }
+
